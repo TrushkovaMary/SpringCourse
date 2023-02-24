cf1a381bb609f1b2c06ae78c1aa7e0e2b33ff4f3
True._.Mary <truefalsemary@gmail.com>
2023-02-24 23:26:32 +0300

    done 9 lesson

--- Lesson4.FirstIoCApp/src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ Lesson4.FirstIoCApp/src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,11 +1,16 @@
 package ru.alishev.springcourse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 /**
  * @author Neil Alishev
  */
+@Component
 public class MusicPlayer {
     private Music music;
 
+    @Autowired
     // IoC
     public MusicPlayer(Music music) {
         this.music = music;
--- Lesson4.FirstIoCApp/src/main/java/ru/alishev/springcourse/TestSpring.java
+++ Lesson4.FirstIoCApp/src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -11,12 +11,16 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("musicBean", Music.class);
+        Music music = context.getBean("rockMusic", Music.class);
 
         MusicPlayer musicPlayer = new MusicPlayer(music);
 
         musicPlayer.playMusic();
 
+        Music music1 = context.getBean("classicalMusic", Music.class);
+        MusicPlayer classicalMusicPlayer = new MusicPlayer(music1);
+        classicalMusicPlayer.playMusic();
+        musicPlayer.playMusic();
         context.close();
     }
 }
